2d621e7bef7d5b4d0e72046b50a15c11e592505d
ellfarnaz <farelnaufalazhari@gmail.com>
2024-06-30 03:10:35 +0700

    Initial Commit

---
+++ belajar-swiftUI/belajar_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  belajar_swiftUIApp.swift
+//  belajar-swiftUI
+//
+//  Created by ellfarnaz on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct belajar_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
